68c2c481dbff135dc5512e6a451e9822f2f4a2ea
James Ware <jazz642@gmail.com>
2018-12-21 14:33:00 -0500

    Added in final parts of project

--- django-quick/mysite/mysite/settings.py
+++ django-quick/mysite/mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '!zz+-yqvr_kz!4%-j=4c$30$vw+zn@7ub7$kz-d$wr5ago9x16'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
